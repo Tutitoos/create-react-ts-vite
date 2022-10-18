9d762563c786083bb7ece34f2a4cda9b94a72bd8
Tutitoos <tutitoosdev@gmail.com>
2022-10-18 09:37:39 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
