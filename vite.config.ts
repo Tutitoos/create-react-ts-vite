36b1d6209e90e5dd2a592d430e4b847c90efc7ae
Tutitoos <tutitoosdev@gmail.com>
2022-10-21 12:00:01 +0200

    Add config testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -7,4 +10,17 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["src/setupTests.ts"],
+    include: ["src/**/*.test.tsx"],
+    exclude: [
+      "src/index.tsx",
+      "src/*.ts",
+      "src/contexts/**",
+      "src/mocks/**",
+      "src/data/**",
+    ],
+  },
 });
